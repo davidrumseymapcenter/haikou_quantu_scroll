521921410a0fc9ea953d889d3f97cdb0b9fb60f2
ect123 <evanthornberry@gmail.com>
2026-06-01 13:02:17 -0700

    adding globe

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/haikou_quantu_scroll/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        globe: resolve(__dirname, "globe/index.html"),
+      },
+    },
+  },
 });
